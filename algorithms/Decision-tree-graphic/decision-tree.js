0efc71d8d8ad72493db414db4463b688ea128a2f
Ann Tech <anntech21@gmail.com>
2023-10-31 14:01:52 -0700

    working on desicion code

--- algorithms/Decision-tree-graphic/decision-tree.js
+++ algorithms/Decision-tree-graphic/decision-tree.js
@@ -1,3 +1,37 @@
+depending on weather =
+If cloudy and am I hungry; I Walk
+If I am not
+then I take bus, 
+return,
+
+if sunny then how I go depends on how much time I have.
+if 30 mins > I bus,
+else 30 mins< walk,
+
+
+
+
+
+
+let weather = x
+let Sun = "Time";
+let Cloud = "Hungry";
+let Rain = "Bus";
+
+
 function transportationOptions() {
+  if (x > 30 mins) {
+    return "Walk";
+} else if (x  Time < 30 mins ) {
+    return "Bus";
+} else if (x Cloud === 0) {
+    return "Walk";}
+    else if {
+      x clould Hungry no)
+      return "Bus";
+    }
+} else {x Rain 
+    return "Bus";
+}
+}
     // Write code here to make the decision tree into an algorithm!
-  }
