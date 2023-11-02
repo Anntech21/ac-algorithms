d47d433d5135cdd69e8ee43722bfd6e95966dfca
Ann Tech <anntech21@gmail.com>
2023-11-02 13:57:35 -0700

    console left

--- algorithms/Decision-tree-graphic/decision-tree.js
+++ algorithms/Decision-tree-graphic/decision-tree.js
@@ -1,37 +1,35 @@
-depending on weather =
-If cloudy and am I hungry; I Walk
-If I am not
-then I take bus, 
-return,
+//depending on weather =
+//If cloudy and am I hungry; I Walk
+//If I am not
+//then I take bus, 
+//return,
 
-if sunny then how I go depends on how much time I have.
-if 30 mins > I bus,
-else 30 mins< walk,
-
-
+//if sunny then how I go depends on how much time I have.
+//if 30 mins > I bus,
+//else 30 mins< walk,
 
 
 
+let weather = "Conditions"; //Declare variable
+let sun = "Time";
+let cloud = "Hunger";
+let rain = "Bus";
 
-let weather = x
-let Sun = "Time";
-let Cloud = "Hungry";
-let Rain = "Bus";
-
-
-function transportationOptions() {
-  if (x > 30 mins) {
+function transportationOptions(weather, sun, cloud, rain) { //Pass variables as parameters
+  if (sun === "Long") { //If sun='Long'
     return "Walk";
-} else if (x  Time < 30 mins ) {
+  } else if (sun === "Short" ) { //If sun='Short'
     return "Bus";
-} else if (x Cloud === 0) {
-    return "Walk";}
-    else if {
-      x clould Hungry no)
-      return "Bus";
-    }
-} else {x Rain 
+  } else if (cloud === "Not hungry") { //If cloud='Not hungry'
     return "Bus";
+  } else if (cloud === "Hungry") { //If cloud='Hungry'
+    return "Walk";
+  } else if (rain === "Rainy") { //If rain='Rainy'
+    return "Bus";
+  }
 }
-}
+
+
+
+
     // Write code here to make the decision tree into an algorithm!
